6c2914db09165e6fb6ce8f84cdf0ef09eb27ff9c
Nitin Mishra <nitinmishra.professional@gmail.com>
2026-08-26 22:23:27 +0530

    chore: remove old files from repository root

--- src/components/common/CategoryBadge.tsx
+++
@@ -1,9 +0,0 @@
-import { DealCategory } from "../../types";
-
-export function CategoryBadge({ category }: { category: DealCategory | string }) {
-  return (
-    <span className="text-[10px] font-bold uppercase tracking-wider text-[var(--signal)]">
-      {category}
-    </span>
-  );
-}
